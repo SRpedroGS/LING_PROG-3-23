dbc4c05d7d9f83916fbac4da46c6257a8fbbd31a
SR_pedro_GS <125514911+SRpedroGS@users.noreply.github.com>
2023-02-22 21:12:23 -0300

    Add files via upload

---
+++ 3cti-pedro/variaveis.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+echo $name = pedro;
+echo "<br>";
+echo $age = 18;
+echo "<hr>";
+$b = true;
+echo gettype(b);
+echo "<hr>";
+
+$nasc = 2006;
+echo gettype($nasc);
+
+
+?> 
---
+++ 3cti-pedro/variavel.php
@@ -0,0 +1,16 @@
+<?php
+
+
+echo $name;
+echo "<br>";
+echo $age;
+echo "<hr>";
+$b = true;
+echo gettype(b);
+echo "<hr>";
+
+$nasc = 2006;
+echo gettype($nasc)
+
+
+?>
